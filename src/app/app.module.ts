d2e36736c668b704a2009cfef6d72b4ac6bfb6a1
dev-allan <allanduminy@protonmail.com>
2021-11-12 14:03:44 +0100

    ajout db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
